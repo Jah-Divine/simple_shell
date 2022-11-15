6e508917890ffa24f1b9cc50e99f951cd28f636e
osikoyaop <optimisticopyso@gmail.com>
2022-11-15 10:41:34 -0800

    Task 0

---
+++ beautiful_code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put fuction
+ *
+ * Description: using the main fuction
+ * this program prints "programming is like building a multilingual puzzle
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}

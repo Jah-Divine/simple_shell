dac6a146d180ab6f0aca5fde147a29dc1656de49
Jah-Divine <ihechidaniella@gmail.com>
2022-11-17 09:05:38 -0800

    Simple shell

--- beautiful_code.c
+++ beautiful_code.c
@@ -1,14 +1,15 @@
+#include "shell.h"
 #include <stdio.h>
 
 /**
  * main - print the string in the put fuction
  *
  * Description: using the main fuction
- * this program prints "programming is like building a multilingual puzzle
+ * this program prints programming is like building a multilingual puzzle
  * Return: 0
  */
 
-int main(void)
+int _putss(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
